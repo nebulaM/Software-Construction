264f91f6c9cd2f0add8d15e8fe54d15c6295c466
nebulaM <nebulaM12@gmail.com>
2017-02-09 17:57:40 -0800

    fix ppt2 indents

--- ppt2/src/beaconsOfGondor/LocateMinasTirith.java
+++ ppt2/src/beaconsOfGondor/LocateMinasTirith.java
@@ -7,31 +7,31 @@ import java.util.Set;
 
 public class LocateMinasTirith {
 
-	/**
-	 * 
-	 * @param numLocations
-	 *            the number of cities in the kingdom, 0 < numCities < 50.
-	 * @param roadNetwork
-	 *            represents the roads connecting different cities in the
-	 *            kingdom. roadNetwork.length( ) is equal to
-	 *            numLocations*numLocations. For all i, j such that 0 <= i <
-	 *            numLocations and 0 <= j < numLocations, the character at index
-	 *            numLocations*i+j in this string indicates if there is a road
-	 *            between cities i and j (1 if there is and 0 if there is no
-	 *            road). For all i such that 0 <= numLocations < i, the
-	 *            character at index numLocations*i+i is 1. <strong>The roadNetwork
-	 *            represents a connected graph.</strong>
-	 * @return a set of city indices with the following condition: the maximum
-	 *         travel time from a city (represented by its index) is as small as
-	 *         possible. The travel time from one city to another is the number
-	 *         of roads to traverse to get from one to the other. For a given
-	 *         city, its maximum travel time is the time to reach that city that
-	 *         is farthest from it in terms of number of roads traversed.
-	 */
-	public static Set<Integer> getGoodLocations(int numLocations, String roadNetwork) {
-	    int minCost=Integer.MAX_VALUE;
-	    Set<Integer> cost=new HashSet<>();
-		for(int i=0;i<numLocations;i++){
+    /**
+     *
+     * @param numLocations
+     *            the number of cities in the kingdom, 0 < numCities < 50.
+     * @param roadNetwork
+     *            represents the roads connecting different cities in the
+     *            kingdom. roadNetwork.length( ) is equal to
+     *            numLocations*numLocations. For all i, j such that 0 <= i <
+     *            numLocations and 0 <= j < numLocations, the character at index
+     *            numLocations*i+j in this string indicates if there is a road
+     *            between cities i and j (1 if there is and 0 if there is no
+     *            road). For all i such that 0 <= numLocations < i, the
+     *            character at index numLocations*i+i is 1. <strong>The roadNetwork
+     *            represents a connected graph.</strong>
+     * @return a set of city indices with the following condition: the maximum
+     *         travel time from a city (represented by its index) is as small as
+     *         possible. The travel time from one city to another is the number
+     *         of roads to traverse to get from one to the other. For a given
+     *         city, its maximum travel time is the time to reach that city that
+     *         is farthest from it in terms of number of roads traversed.
+     */
+    public static Set<Integer> getGoodLocations(int numLocations, String roadNetwork) {
+        int minCost=Integer.MAX_VALUE;
+        Set<Integer> cost=new HashSet<>();
+        for(int i=0;i<numLocations;i++){
             int tempCost=0;
             for(char j=0;j<numLocations;j++) {
                 if(j!=i){
@@ -49,8 +49,8 @@ public class LocateMinasTirith {
                 cost.add(i);
             }
         }
-		return cost;
-	}
+        return cost;
+    }
 
     /**
      * breath first search to get cost from start to end
@@ -60,36 +60,36 @@ public class LocateMinasTirith {
      * @param networkSize how many nodes
      * @return cost from start to end
      */
-	private static int cost(int start, int end, String roadNetwork, int networkSize){
-		Queue<Integer> q=new LinkedList<>();
-		Set<Integer> visited=new HashSet<>();
+    private static int cost(int start, int end, String roadNetwork, int networkSize){
+        Queue<Integer> q=new LinkedList<>();
+        Set<Integer> visited=new HashSet<>();
         //array tracks node parent, index represents node, content represents parent of node
         int[] parent=new int[networkSize];
-		int cost=1;
-		q.add(start);
-		while(!q.isEmpty()){
-			int current=q.remove();
-			if(current==end){
-			    //add up cost until we reach start
-			    while(parent[current]!=start){
-			        //cost is always 1
-			        cost++;
+        int cost=1;
+        q.add(start);
+        while(!q.isEmpty()){
+            int current=q.remove();
+            if(current==end){
+                //add up cost until we reach start
+                while(parent[current]!=start){
+                    //cost is always 1
+                    cost++;
                     current=parent[current];
                 }
-				return cost;
-			}
-			for(int i=0;i<networkSize;i++){
-			    //location [i][current] in roadNetwork is index
-				int index= i*networkSize+current;
-				if(roadNetwork.charAt(index)=='1'){
-					if(!visited.contains(i)){
-						visited.add(i);
-						q.add(i);
-						parent[i]=current;
-					}
-				}
-			}
-		}
-		return cost;
-	}
+                return cost;
+            }
+            for(int i=0;i<networkSize;i++){
+                //location [i][current] in roadNetwork is index
+                int index= i*networkSize+current;
+                if(roadNetwork.charAt(index)=='1'){
+                    if(!visited.contains(i)){
+                        visited.add(i);
+                        q.add(i);
+                        parent[i]=current;
+                    }
+                }
+            }
+        }
+        return cost;
+    }
 }
--- ppt2/src/fibonacciTransform/FibTransform.java
+++ ppt2/src/fibonacciTransform/FibTransform.java
@@ -2,27 +2,27 @@ package fibonacciTransform;
 
 public class FibTransform {
 
-	/**
-	 * Transform a number into a Fibonacci number using at most m steps: Of
-	 * these steps, one can be a doubling step, and the other steps increase
-	 * the number at hand by adding 1.
-	 * 
-	 * We are only interested in Fibonacci numbers that can represented as
-	 * Integer or int.
-	 * 
-	 * @param n
-	 *            the number to transform to a Fibonacci number, n >= 0
-	 * @param m
-	 *            the number of transformation steps permitted, m >= 0
-	 * @return true if the transformation is possible and false otherwise
-	 */
-	public static boolean isPossible_onlyOneDoubling(int n, int m) {
-		return n>=0 && isPossible_onlyOneDoublingHelper(n,m,false);
-	}
+    /**
+     * Transform a number into a Fibonacci number using at most m steps: Of
+     * these steps, one can be a doubling step, and the other steps increase
+     * the number at hand by adding 1.
+     *
+     * We are only interested in Fibonacci numbers that can represented as
+     * Integer or int.
+     *
+     * @param n
+     *            the number to transform to a Fibonacci number, n >= 0
+     * @param m
+     *            the number of transformation steps permitted, m >= 0
+     * @return true if the transformation is possible and false otherwise
+     */
+    public static boolean isPossible_onlyOneDoubling(int n, int m) {
+        return n>=0 && isPossible_onlyOneDoublingHelper(n,m,false);
+    }
 
-	private static boolean isPossible_onlyOneDoublingHelper(int n, int m, boolean doubled){
-	    if(m<=0){
-	        return isFib(n);
+    private static boolean isPossible_onlyOneDoublingHelper(int n, int m, boolean doubled){
+        if(m<=0){
+            return isFib(n);
         }
         if(doubled) {
             return isFib(n) || isPossible_onlyOneDoublingHelper(n+1,m-1,doubled);
@@ -44,28 +44,28 @@ public class FibTransform {
         return fib2 == n;
     }
 
-	/**
-	 * Transform a number into a Fibonacci number using at most m steps: One can
-	 * use any sequence of doubling (*2) or addition (+1) steps as long as the
-	 * total number of steps used is no more than m.
-	 * 
-	 * We are only interested in Fibonacci numbers that can represented as
-	 * Integer or int.
-	 * 
-	 * @param n
-	 *            the number to transform to a Fibonacci number, n >= 0
-	 * @param m
-	 *            the number of transformation steps permitted, m >= 0
-	 * @return true if the transformation is possible and false otherwise
-	 */
-	public static boolean isPossible(int n, int m) {
-		if(n<0){
-		    return false;
+    /**
+     * Transform a number into a Fibonacci number using at most m steps: One can
+     * use any sequence of doubling (*2) or addition (+1) steps as long as the
+     * total number of steps used is no more than m.
+     *
+     * We are only interested in Fibonacci numbers that can represented as
+     * Integer or int.
+     *
+     * @param n
+     *            the number to transform to a Fibonacci number, n >= 0
+     * @param m
+     *            the number of transformation steps permitted, m >= 0
+     * @return true if the transformation is possible and false otherwise
+     */
+    public static boolean isPossible(int n, int m) {
+        if(n<0){
+            return false;
         }
         if(m<=0){
             return isFib(n);
         }
-		return isFib(n) || isPossible(n+1,m-1) || isPossible(n<<1,m-1);
-	}
+        return isFib(n) || isPossible(n+1,m-1) || isPossible(n<<1,m-1);
+    }
 
 }
