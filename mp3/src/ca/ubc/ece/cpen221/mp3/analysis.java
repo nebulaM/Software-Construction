11f7adf11fa60fec732bc1e087ff40ddd50a2f23
nebulam <nebulam12@gmail.com>
2017-02-21 14:41:56 -0800

    mp3 fix String outOfBound; improve query

--- mp3/src/ca/ubc/ece/cpen221/mp3/analysis.java
+++ mp3/src/ca/ubc/ece/cpen221/mp3/analysis.java
@@ -1,6 +1,5 @@
 package ca.ubc.ece.cpen221.mp3;
 
-
 import ca.ubc.ece.cpen221.mp3.graph.AdjacencyListGraph;
 import ca.ubc.ece.cpen221.mp3.graph.AdjacencyMatrixGraph;
 import ca.ubc.ece.cpen221.mp3.graph.Algorithms;
@@ -13,7 +12,6 @@ import java.net.URL;
 import java.util.*;
 
 public class analysis {
-    static Map<String, Vertex> track=new TreeMap<>();
     public static void main(String[] args) throws IOException{
         Graph mGraph=new AdjacencyMatrixGraph();
         Scanner console= new Scanner(System.in);
@@ -21,39 +19,41 @@ public class analysis {
         boolean loopMain=true;
         while(true){
             loopMain=true;
-            if(choice.charAt(0)=='l'||choice.equals("load")){
-                System.out.println("load in data, enter path of the data: ");
-                String url=console.nextLine().trim();
-                if(url.length()>0){
-                    if(isURL(url)){
-                        System.out.println("Downloading from " + url + " ...");
-                    }
-                    try{
-                        mGraph=processGraphFromFileOrURL(url);
-                        printGraph(mGraph);
+            if(choice.length()>0) {
+                if (choice.charAt(0) == 'l' || choice.equals("load")) {
+                    System.out.println("load in data, enter path of the data: ");
+                    String url = console.nextLine().trim();
+                    if (url.length() > 0) {
+                        if (isURL(url)) {
+                            System.out.println("Downloading from " + url + " ...");
+                        }
+                        try {
+                            mGraph = processGraphFromFileOrURL(url);
+                            printGraph(mGraph);
 
-                    }catch (MalformedURLException mfurle){
-                        System.out.println("Badly formatted URL: " + url);
-                    }catch (FileNotFoundException fnfe) {
-                        System.out.println("Web page or file not found: " + url);
-                    } catch (IOException ioe) {
-                        System.out.println("I/O error: " + ioe.getMessage());
+                        } catch (MalformedURLException mfurle) {
+                            System.out.println("Badly formatted URL: " + url);
+                        } catch (FileNotFoundException fnfe) {
+                            System.out.println("Web page or file not found: " + url);
+                        } catch (IOException ioe) {
+                            System.out.println("I/O error: " + ioe.getMessage());
+                        }
+                    } else {
+                        System.out.println("url cannot be empty");
                     }
-                }else{
-                    System.out.println("url cannot be empty");
+                } else if (choice.charAt(0) == 'p' || choice.equals("print")) {
+                    printGraph(mGraph);
+                } else if (choice.charAt(0) == 'q' || choice.equals("query")) {
+                    if (mGraph.size() == 0) {
+                        System.out.println("Please load data into graph first");
+                        choice = "load";
+                        loopMain = false;
+                    } else {
+                        queryGraph(mGraph, console);
+                    }
+                } else if (choice.equals("quit")) {
+                    break;
                 }
-            }else if(choice.charAt(0)=='p'||choice.equals("print")){
-                printGraph(mGraph);
-            }else if(choice.charAt(0)=='q'||choice.equals("query")){
-                if(mGraph.size()==0){
-                    System.out.println("Please load data into graph first");
-                    choice="load";
-                    loopMain=false;
-                }else{
-                    queryGraph(mGraph,console);
-                }
-            }else if(choice.equals("quit")){
-                break;
             }
             if(loopMain) {
                 System.out.println("(l)oad, (p)rint, (q)uery, (h)elp, quit?");
@@ -101,10 +101,12 @@ public class analysis {
         while ((line=reader.readLine())!=null) {
             String noSpace=line.trim();
             String[] ids=noSpace.split("->");
+            //trim is necessary!!!
             Vertex v0=new Vertex(ids[0].trim());
             Vertex v1=new Vertex(ids[1].trim());
             graph.addVertex(v0);
             graph.addVertex(v1);
+            //v0 -> v1 means v1 follows v0
             graph.addEdge(v1,v0);
         }
         System.out.println("Finished reading data into graph");
@@ -128,28 +130,31 @@ public class analysis {
         System.out.println("quit to go to upper level");
         while(true) {
             String in = sn.nextLine();
-            Vertex v0=new Vertex("");
-            Vertex v1=new Vertex("");
-            if (in.endsWith("?")) {
-                String[] args = in.split(" ");
-                System.out.println("args[1] "+ args[1]);
-                System.out.println("args[2] "+ args[2]);
-                v0 = new Vertex(args[1]);
-                v1 = new Vertex(args[2]);
-                if (args[0].equals("ci")) {
-                    System.out.println("commonInfluencers " + Algorithms.commonDownstreamVertices(graph, v0, v1));
-                } else if (args[0].equals("nr")) {
-                    System.out.println("numRetweets " + Algorithms.shortestDistance(graph, v0, v1));
+            if(in.length()>0) {
+                if (in.endsWith("?")) {
+                    String[] args = in.split(" ");
+                    System.out.println("args[1] " + args[1]);
+                    System.out.println("args[2] " + args[2]);
+                    if (args[0].equals("ci")) {
+                        System.out.println("commonInfluencers " + Algorithms.commonDownstreamVertices(graph, new Vertex(args[1].trim()), new Vertex(args[2].trim())));
+                    } else if (args[0].equals("nr")) {
+                        int cost=Algorithms.shortestDistance(graph, new Vertex(args[1].trim()), new Vertex(args[2].trim()));
+                        if(cost==Algorithms.NO_EDGE){
+                            System.out.println("retweet from "+args[1]+" to "+args[2]+" is not possible");
+                        }
+                        else if(cost>0){//cost =0 means args[1] follows args[2]
+                            //[re]tweet, so exclude the first tweet
+                            cost--;
+                        }
+                        System.out.println("numRetweets " + cost);
+                    }
+                } else if (in.equals("print")) {
+                    printGraph(graph);
+                } else if (in.equals("quit")) {
+                    break;
+                } else {
+                    System.out.println("invalid input");
                 }
-            }else if(in.equals("print")){
-                printGraph(graph);
-                System.out.println("down v0 "+graph.getDownstreamNeighbors(v0));
-                System.out.println("down v1 "+graph.getDownstreamNeighbors(v1));
-            }
-            else if(in.equals("quit")){
-                break;
-            }else {
-                System.out.println("invalid input");
             }
         }
     }
