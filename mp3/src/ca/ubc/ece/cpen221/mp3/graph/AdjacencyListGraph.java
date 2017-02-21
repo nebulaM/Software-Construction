24c4d32681ae56916970c2a75406e4b779e6709e
nebulam <nebulam12@gmail.com>
2017-02-21 14:25:08 -0800

    mp3 update graph ADTs

--- mp3/src/ca/ubc/ece/cpen221/mp3/graph/AdjacencyListGraph.java
+++ mp3/src/ca/ubc/ece/cpen221/mp3/graph/AdjacencyListGraph.java
@@ -11,44 +11,48 @@ import java.util.HashMap;
 
 public class AdjacencyListGraph implements Graph {
     private Map<Vertex,List<Vertex>> mGraph;
+    private static long mGraphSize;
     public AdjacencyListGraph(){
         mGraph=new HashMap<>();
+        mGraphSize=0;
+    }
+
+    @Override
+    public int size(){
+        return (int)mGraphSize;
     }
 
     @Override
     public void addVertex(Vertex v){
         if(!mGraph.containsKey(v)){
             mGraph.put(v,new ArrayList<>());
-        }else {
+            mGraphSize++;
+        }/*else {
             System.out.println("@addVertex: Vertex "+v.toString()+" already in graph");
-        }
+        }*/
     }
 
     @Override
     public void addEdge(Vertex v1, Vertex v2){
         if(!v1.equals(v2) && mGraph.containsKey(v1) && mGraph.containsKey(v2)){
             mGraph.get(v1).add(v2);
-        }else{
+        }/*else{
             if(v1.equals(v2)){
                 System.out.println("@addEdge: Vertex v1 " + v1.toString() + " equals to v2");
             }else {
                 System.out.println("@addEdge: Vertex v1 or Vertex v2 not in graph");
             }
-        }
+        }*/
     }
 
     @Override
     public boolean edgeExists(Vertex v1, Vertex v2){
-        return mGraph.get(v1).contains(v2);
+        return v1!=null && v2!=null && mGraph.containsKey(v1)&&mGraph.get(v1).contains(v2);
     }
 
     @Override
     public List<Vertex> getDownstreamNeighbors(Vertex v){
-        if(mGraph.containsKey(v)){
-            return mGraph.get(v);
-        }else {
-            return new ArrayList<>();
-        }
+        return mGraph.get(v);
     }
 
     @Override
--- mp3/src/ca/ubc/ece/cpen221/mp3/graph/AdjacencyMatrixGraph.java
+++ mp3/src/ca/ubc/ece/cpen221/mp3/graph/AdjacencyMatrixGraph.java
@@ -6,56 +6,57 @@ import ca.ubc.ece.cpen221.mp3.staff.Vertex;
 import java.util.*;
 
 public class AdjacencyMatrixGraph implements Graph {
-private Map<Vertex,Set<Vertex>> mGraph;
+    private Map<Vertex,Set<Vertex>> mGraph;
+    private static long mGraphSize;
     public AdjacencyMatrixGraph(){
         mGraph=new HashMap<>();
+        mGraphSize=0;
+    }
+
+    @Override
+    public int size(){
+        return (int)mGraphSize;
     }
 
     @Override
     public void addVertex(Vertex v){
         if(!mGraph.containsKey(v)){
+            //find an element in HashSet has time complexity O(1), which simulates matrix behavioural
             mGraph.put(v,new HashSet<>());
-        }else {
+            mGraphSize++;
+        }/*else {
             System.out.println("@addVertex: Vertex "+v.toString()+" already in graph");
-        }
+        }*/
     }
 
     @Override
     public void addEdge(Vertex v1, Vertex v2){
         if(!v1.equals(v2) && mGraph.containsKey(v1) && mGraph.containsKey(v2)){
             mGraph.get(v1).add(v2);
-        }else{
+        }/*else{
             if(v1.equals(v2)){
                 System.out.println("@addEdge: Vertex v1 " + v1.toString() + " equals to v2");
             }else {
                 System.out.println("@addEdge: Vertex v1 or Vertex v2 not in graph");
             }
-        }
+        }*/
     }
 
     @Override
     public boolean edgeExists(Vertex v1, Vertex v2){
-        return mGraph.get(v1).contains(v2);
+        return v1!=null && v2!=null && mGraph.containsKey(v1)&&mGraph.get(v1).contains(v2);
     }
 
     @Override
     public List<Vertex> getDownstreamNeighbors(Vertex v){
-        if(mGraph.containsKey(v)){
-            List downStreamList=new ArrayList();
-            for(Vertex dv:mGraph.get(v)){
-                downStreamList.add(dv);
-            }
-            return downStreamList;
-        }else {
-            return new ArrayList<>();
-        }
+        return new ArrayList<>(mGraph.get(v));
     }
 
     @Override
     public List<Vertex> getUpstreamNeighbors(Vertex v){
         List<Vertex> list=new ArrayList<>();
-        for(Vertex key: mGraph.keySet()){
-            if(mGraph.get(key).contains(v)){
+        for (Vertex key : mGraph.keySet()) {
+            if (mGraph.get(key).contains(v)) {
                 list.add(key);
             }
         }
